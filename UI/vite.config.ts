1f4ca9a517ac0f188c09f1f340ca80749c9f5dfe
Praveen-R_mrcooper <praveen.r@mrcooper.com>
2025-10-05 12:34:24 +0530

    addition to routes

--- UI/vite.config.ts
+++ UI/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       output: {
         manualChunks: undefined,
